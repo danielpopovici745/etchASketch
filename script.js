0b79f217ef6dfd313153a6179c12ee23ebca4efa
Daniel Popovici <dpopovici601@gmail.com>
2022-07-21 13:50:09 -0400

    created a button that allows the user to pick how many tiles per side they would like in their grid. added some exception handling to the user prompt. still generates a blank grid if they enter a value that isn't a number

--- script.js
+++ script.js
@@ -1,24 +1,66 @@
 // Daniel Popovici - script.js 7/21/2022
 
-let pageContainer = document.querySelector("#pageContainer");
-let buttonContainer = document.querySelector("#buttonContainer");
-let grid = document.createElement("div");
+const pageContainer = document.querySelector("#pageContainer");
+const buttonContainer = document.querySelector("#buttonContainer");
+const grid = document.createElement("div");
 grid.setAttribute("id","grid");
 pageContainer.appendChild(grid);
-
-for(let i = 1;i<=16;i++){
-    let row = document.createElement("div");
-    row.setAttribute("class",`row`);
-    grid.appendChild(row);
-    for(let i =1; i<=16;i++){
-        let square = document.createElement("div");
-        square.setAttribute("class","square");
-        row.appendChild(square);
+let sides = 16;
+function createGrid(sides){
+    for(let i = 1;i<=sides;i++){
+        let row = document.createElement("div");
+        row.setAttribute("class",`row`);
+        grid.appendChild(row);
+        for(let i =1; i<=sides;i++){
+            let square = document.createElement("div");
+            square.setAttribute("class","square");
+            row.appendChild(square);
+        }
     }
+    squares = document.querySelectorAll(".square");
+    giveTilesMouseOver(squares);
 }
-const squares = document.querySelectorAll(".square");
-squares.forEach(sq => sq.addEventListener('mouseover',() => sq.setAttribute("style","background-color:#000000; transition: background-color ease 0.3s;"))
-);
+
+function destroyGrid(){
+    squares.forEach(sq => sq.remove());
+    const rows = document.querySelectorAll(".row");
+    rows.forEach(row => 
+        row.remove()
+    )
+}
+
+function giveTilesMouseOver(){
+    
+    squares.forEach(sq => sq.addEventListener('mouseover',() => sq.setAttribute("style","background-color:#000000; transition: background-color ease 0.3s;"))
+    );
+}
+
+createGrid(sides);
+
+
+const howManySquaresButton = document.createElement('button');
+howManySquaresButton.textContent = '# of Tiles per Side';
+howManySquaresButton.addEventListener('click',function(){
+    sides = prompt("How many tiles per side would you like? (Enter an integer between 0 and 48");
+    if(sides > 47){
+        while(sides > 47){
+            sides = prompt("Sorry,this many tiles will make the grid disappear. Please enter an integer smaller than 48");
+        }
+        
+    }
+    if(sides == undefined || sides == 0){
+        while(sides == undefined || sides == 0){
+            sides = prompt("How many tiles per side would you like? (Enter an integer between 0 and 48");
+        }
+        
+    }
+    
+    
+    destroyGrid();
+    createGrid(sides);
+});
+buttonContainer.appendChild(howManySquaresButton);
+
 
 const resetGrid = document.createElement("button");
 resetGrid.setAttribute("id","resetGrid");
@@ -28,8 +70,6 @@ resetGrid.addEventListener('click',function(){
 });
 buttonContainer.appendChild(resetGrid);
 
-const howManySquaresButton = document.createElement('button');
-howManySquaresButton.textContent = 'How many squares?';
-buttonContainer.appendChild(howManySquaresButton);
+
 
 
