345a07489a0cf22c87bb497a08f6d9f9d2716fe8
Daniel Popovici <dpopovici601@gmail.com>
2022-07-21 12:54:16 -0400

    created a reset button that resets the grid to all white backgrounds. also styled the button as well

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 // Daniel Popovici - script.js 7/21/2022
 
 let pageContainer = document.querySelector("#pageContainer");
+let buttonContainer = document.querySelector("#buttonContainer");
 let grid = document.createElement("div");
 grid.setAttribute("id","grid");
 pageContainer.appendChild(grid);
@@ -16,6 +17,19 @@ for(let i = 1;i<=16;i++){
     }
 }
 const squares = document.querySelectorAll(".square");
-console.log(squares);
-squares.forEach(sq => sq.addEventListener('mouseover',() => sq.setAttribute("style","background-color:black; transition: background-color ease 0.3s;"))
+squares.forEach(sq => sq.addEventListener('mouseover',() => sq.setAttribute("style","background-color:#000000; transition: background-color ease 0.3s;"))
 );
+
+const resetGrid = document.createElement("button");
+resetGrid.setAttribute("id","resetGrid");
+resetGrid.textContent = 'RESET';
+resetGrid.addEventListener('click',function(){
+    squares.forEach(sq => sq.setAttribute("style","background-color:#ffffff; transition: background-color ease 0.3s"))
+});
+buttonContainer.appendChild(resetGrid);
+
+const howManySquaresButton = document.createElement('button');
+howManySquaresButton.textContent = 'How many squares?';
+buttonContainer.appendChild(howManySquaresButton);
+
+
